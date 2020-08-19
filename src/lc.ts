6f20ce47e5712e1dff49d52a33b5f57c3cb94376
Matei Adriel <rafaeladriel11@gmail.com>
2020-08-19 15:32:16 +0300

    feat: new indexed ast stuff

--- src/lc.ts
+++ src/lc.ts
@@ -1,63 +1,23 @@
 import type { ADT } from 'ts-adt'
 import type { Fn } from '@thi.ng/api'
 
-export type Lambda<T> = {
-  argument: string
-  body: Ast<T>
-}
-
-export type Program<T = {}> = {
-  arguments: string[]
-  body: Ast<T>
-}
-
-export type Var = { name: string }
-export type Call<T> = { func: Ast<T>; argument: Ast<T> }
+/**
+ * Base ast type. This is what we can convert other representations to.
+ */
+export type Ast = ADT<{
+  call: { func: Ast; argument: Ast }
+  lambda: { argument: string; body: Ast }
+  var: { name: string }
+}>
 
 /**
- * Our own weird representation for the lambda calculus. Designed for ease of rendering.
+ * Modified ast which indexes everything with an id.
  */
-export type Ast<T> = T &
-  ADT<{
-    // let: { name: string; value: Ast; in: Ast }
-    call: Call<T>
-    lambda: Lambda<T>
-    var: Var
-  }>
-
-/**
- * type which has an unique id.
- */
-type IId<T> = { id: T }
-
-/**
- * Run a function on each node of an ast.
- *
- * @param f The function to run on each layer.
- */
-const everywhereOnAst = <T, U = {}>(f: Fn<Ast<T>, Ast<T> & U>) => (
-  ast: Ast<T>
-): Ast<U> => {
-  const updated = f(ast)
-  const go = everywhereOnAst(f)
-
-  if (updated._type === 'call') {
-    return {
-      ...updated,
-      argument: go(updated.argument),
-      func: go(updated.func)
-    }
-  }
-
-  if (updated._type === 'lambda') {
-    return {
-      ...updated,
-      body: go(updated.body)
-    }
-  }
-
-  return updated
-}
+export type IndexedAst = ADT<{
+  call: { func: IndexedAst; argument: IndexedAst }
+  lambda: { argument: string; argumentId: number; body: IndexedAst }
+  var: { name: string }
+}> & { id: number }
 
 /**
  * Add an id to every node in the ast.
@@ -65,22 +25,50 @@ const everywhereOnAst = <T, U = {}>(f: Fn<Ast<T>, Ast<T> & U>) => (
  * @param ast The ast to add ids to.
  * @param ids Generator for the actual ids.
  */
-export const withIds = <T = {}, I = number>(
-  ast: Ast<T>,
-  ids: Generator<I, void>
-): Ast<T & IId<I>> => {
-  const go = everywhereOnAst<T, T & IId<I>>((expression) => {
+export const indexAst = (
+  ast: Ast,
+  ids: Generator<number, void>,
+  scope: Record<string, number> = {}
+): IndexedAst => {
+  const getId = () => {
     const id = ids.next()
 
-    if (id.done) {
+    if (id.done)
       throw new Error(`Cannot generate enough ids for the entire ast`)
-    }
+
+    return id.value
+  }
+
+  if (ast._type === 'var') {
+    if (!Reflect.has(scope, ast.name))
+      throw new Error(`Variable ${ast.name} not in scope.`)
 
     return {
-      ...expression,
-      id: id.value
+      ...ast,
+      id: scope[ast.name]
     }
-  })
+  }
 
-  return go(ast)
+  if (ast._type === 'call') {
+    return {
+      _type: 'call',
+      argument: indexAst(ast.argument, ids, scope),
+      func: indexAst(ast, ids, scope),
+      id: getId()
+    }
+  }
+
+  if (ast._type === 'lambda') {
+    const argumentId = getId()
+
+    return {
+      _type: 'lambda',
+      argument: ast.argument,
+      id: getId(),
+      argumentId,
+      body: indexAst(ast.body, ids, { ...scope, [ast.argument]: argumentId })
+    }
+  }
+
+  throw new Error(`Ast ${ast} has an unknown type.`)
 }
